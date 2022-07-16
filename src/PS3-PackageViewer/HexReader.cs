ec5cf51b0841907f8f7b95fa6d92255354a0a0df
coreizer <coreizer@outlook.com>
2022-07-16 10:30:23 +0900

    BeforePosition プロパティ追加

--- src/PS3-PackageViewer/HexReader.cs
+++ src/PS3-PackageViewer/HexReader.cs
@@ -23,7 +23,13 @@ namespace PS3_PackageViewer
          }
       }
 
-      private long _lastOffset;
+      private long _beforePosition;
+      public long BeforePosition
+      {
+         get {
+            return this._beforePosition;
+         }
+      }
 
       public Endianness Endian { get; set; } = Endianness.Little;
 
@@ -43,12 +49,20 @@ namespace PS3_PackageViewer
       public HexReader(FileStream fileStream) : this(fileStream, Endianness.Little)
       { }
 
+      public HexReader(FileStream fileStream, Endianness Endian)
+      {
+         fileStream.Seek(0, SeekOrigin.Begin);
+         this.BaseStream = fileStream;
+         this.Endian = Endian;
+         this._beforePosition = fileStream.Position;
+      }
+
       public HexReader(Stream stream, Endianness Endian)
       {
          stream.Seek(0, SeekOrigin.Begin);
          this.BaseStream = stream;
          this.Endian = Endian;
-         this._lastOffset = stream.Position;
+         this._beforePosition = stream.Position;
       }
 
       public sbyte ReadSByte()
@@ -58,22 +72,22 @@ namespace PS3_PackageViewer
 
       public sbyte ReadSByte(long offset)
       {
-         return (sbyte)this.ReadBytes(1, offset)[0];
+         return (sbyte)this.ReadCore(1, offset)[0];
       }
 
       public byte ReadByte()
       {
-         return this.ReadByte(this.Position);
+         return this.ReadCore(1)[0];
       }
 
       public byte ReadByte(long offset)
       {
-         return this.ReadBytes(1, offset)[0];
+         return this.ReadCore(1, offset)[0];
       }
 
       public byte[] ReadBytes(int length)
       {
-         return ReadCore(length);
+         return this.ReadCore(length);
       }
 
       public byte[] ReadBytes(int length, Endianness endian)
@@ -81,24 +95,24 @@ namespace PS3_PackageViewer
          return ReadCore(length, endian: endian, priorityArgument: true);
       }
 
-      public byte[] ReadBytes(int length, long offset)
+      public byte[] ReadBytes(int length, long offset, Endianness endian = Endianness.Little)
       {
-         return ReadCore(length, offset);
+         return ReadCore(length, offset, endian, true);
       }
 
       public short ReadShort()
       {
-         return BitConverter.ToInt16(this.ReadBytes(2, this._lastOffset), 0);
+         return BitConverter.ToInt16(this.ReadCore(2), 0);
       }
 
       public short ReadShort(long offset)
       {
-         return BitConverter.ToInt16(this.ReadBytes(2, offset), 0);
+         return BitConverter.ToInt16(this.ReadCore(2, offset), 0);
       }
 
       public ushort ReadUShort()
       {
-         return BitConverter.ToUInt16(this.ReadCore(2, this._lastOffset), 0);
+         return BitConverter.ToUInt16(this.ReadCore(2), 0);
       }
 
       public ushort ReadUShort(Endianness endian)
@@ -113,47 +127,47 @@ namespace PS3_PackageViewer
 
       public int ReadInt32()
       {
-         return BitConverter.ToInt32(this.ReadBytes(4, this._lastOffset), 0);
+         return BitConverter.ToInt32(this.ReadCore(4), 0);
       }
 
       public int ReadInt32(long offset)
       {
-         return BitConverter.ToInt32(this.ReadBytes(4, offset), 0);
+         return BitConverter.ToInt32(this.ReadCore(4, offset), 0);
       }
 
       public uint ReadUInt32()
       {
-         return BitConverter.ToUInt32(this.ReadBytes(4, this._lastOffset), 0);
+         return BitConverter.ToUInt32(this.ReadCore(4), 0);
       }
 
       public uint ReadUInt32(long offset)
       {
-         return BitConverter.ToUInt32(this.ReadBytes(4, offset), 0);
+         return BitConverter.ToUInt32(this.ReadCore(4, offset), 0);
       }
 
       public long ReadInt64()
       {
-         return BitConverter.ToInt64(this.ReadBytes(8, this._lastOffset), 0);
+         return BitConverter.ToInt64(this.ReadCore(8), 0);
       }
 
       public long ReadInt64(long offset)
       {
-         return BitConverter.ToInt64(this.ReadBytes(8, offset), 0);
+         return BitConverter.ToInt64(this.ReadCore(8, offset), 0);
       }
 
       public ulong ReadUInt64()
       {
-         return BitConverter.ToUInt64(this.ReadBytes(8, this._lastOffset), 0);
+         return BitConverter.ToUInt64(this.ReadCore(8), 0);
       }
 
       public ulong ReadUInt64(long offset)
       {
-         return BitConverter.ToUInt64(this.ReadBytes(8, offset), 0);
+         return BitConverter.ToUInt64(this.ReadCore(8, offset), 0);
       }
 
       public float ReadFloat(long offset)
       {
-         return BitConverter.ToSingle(this.ReadBytes(4, offset), 0);
+         return BitConverter.ToSingle(this.ReadCore(4, offset), 0);
       }
 
       public string ReadString(int length)
@@ -175,8 +189,8 @@ namespace PS3_PackageViewer
          if (offset >= 0) {
             this.BaseStream.Position = offset;
          }
+         this._beforePosition = this.BaseStream.Position;
          this.BaseStream.Read(buffer, 0, buffer.Length);
-         this._lastOffset = this.BaseStream.Position;
          if (this.Endian == Endianness.Big && !priorityArgument || endian == Endianness.Big) {
             Array.Reverse(buffer, 0, buffer.Length);
          }
